30575eb98045af93cebd2ff486c94962ba1e7d31
Lesh <11671272+derLesh@users.noreply.github.com>
2019-03-14 22:27:55 +0100

    Update twitchController.js

--- api/models/controllers/twitchController.js
+++ api/models/controllers/twitchController.js
@@ -4,3 +4,7 @@ var MD5 = require('md5.js');
 exports.welcome = function(req, res) {
     res.send("helo pepeL");
 };
+
+exports.random = function(req, res) {
+    res.send(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15););   
+}
