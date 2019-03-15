a23bf482cb531a9ce6bbe9cc1c99a5a0e76570ee
Lesh <11671272+derLesh@users.noreply.github.com>
2019-03-15 15:58:04 +0100

    Updated file to the last stable version

--- api/routes/twitchRoute.js
+++ api/routes/twitchRoute.js
@@ -2,10 +2,6 @@
 
 module.exports = function(app) {
     var twitch = require("../controllers/twitchController");
-
-    // twitch routes
-    app.route("/twitch")
-        .get(twitch.welcome);
     
     app.route("/twitch/random")
         .get(twitch.random);
